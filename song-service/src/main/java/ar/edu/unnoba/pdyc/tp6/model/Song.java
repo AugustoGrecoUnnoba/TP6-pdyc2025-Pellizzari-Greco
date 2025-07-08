e7ab6da14a3cca7270aaf7f8d039fbb2778ec75b
grekis <augus_greco@hotmail.com>
2025-07-08 10:26:31 -0300

    Add song service implementation with repository and DTO

---
+++ song-service/src/main/java/ar/edu/unnoba/pdyc/tp6/model/Song.java
@@ -0,0 +1,43 @@
+package ar.edu.unnoba.pdyc.tp6.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "songs")
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Constructores
+    public Song() {
+    }
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
